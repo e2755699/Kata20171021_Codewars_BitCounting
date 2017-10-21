661f5a773b22df1efe334c89e96003d4f98e877a
Dustin <e275566@gmail.com>
2017-10-21 17:45:27 +0800

    first version

--- Kata20171021_Codewars_BitCounting/Kata.cs
+++ Kata20171021_Codewars_BitCounting/Kata.cs
@@ -15,19 +15,67 @@ namespace Kata20171021_Codewars_BitCounting
         }
 
         [TestMethod]
-        public void Input_2_ShouldReturn_2()
+        public void Input_2_ShouldReturn_1()
         {
             var input =2;
+            var expect = 1;
+            AssertShoudBe(expect, input);
+        }
+
+        [TestMethod]
+        public void Input_3_ShouldReturn_2()
+        {
+            var input = 3;
             var expect = 2;
             AssertShoudBe(expect, input);
         }
 
+        [TestMethod]
+        public void Input_4_ShouldReturn_1()
+        {
+            var input = 4;
+            var expect = 1;
+            AssertShoudBe(expect, input);
+        }
+
+        [TestMethod]
+        public void Input_5_ShouldReturn_2()
+        {
+            var input = 5;
+            var expect = 2;
+            AssertShoudBe(expect, input);
+        }
+
+        [TestMethod]
+        public void Input_8_ShouldReturn_1()
+        {
+            var input = 8;
+            var expect = 1;
+            AssertShoudBe(expect, input);
+        }
+
+        [TestMethod]
+        public void Input_7_ShouldReturn_1()
+        {
+            var input = 7;
+            var expect = 3;
+            AssertShoudBe(expect, input);
+        }
+
+
+        [TestMethod]
+        public void Input_0_ShouldReturn_0()
+        {
+            var input = 0;
+            var expect = 0;
+            AssertShoudBe(expect, input);
+        }
 
         private void AssertShoudBe(int expect, int input)
         {
             var BC = new BitCounting();
             int actual = BC.getNumberOfBit(input);
-            Assert.AreEqual(expect, input);
+            Assert.AreEqual(expect, actual);
         }
     }
 
@@ -35,7 +83,15 @@ namespace Kata20171021_Codewars_BitCounting
     {
         public int getNumberOfBit(int input)
         {
-            return input;
+            var count = 0;
+            for (var i = input; i > 0; i = i / 2)
+            {
+                if (i % 2 == 1)
+                {
+                    count++;
+                }
+            }
+            return count;
         }
     }
 }
