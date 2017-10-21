a167a82acc503afc56bd798a7a1d92b19e13508b
Dustin <e275566@gmail.com>
2017-10-21 16:50:56 +0800

    加入專案檔案。

---
+++ Kata20171021_Codewars_BitCounting/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Kata20171021_Codewars_BitCounting
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
