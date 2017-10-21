ee24f4c3d7f87f5eed09ebaa01b90fa009edaa67
Dustin <e275566@gmail.com>
2017-10-21 17:19:58 +0800

    For test case "Input_2_ShouldReturn_2"

---
+++ Kata20171021_Codewars_BitCounting/Kata.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Kata20171021_Codewars_BitCounting
+{
+    [TestClass]
+    public class Kata
+    {
+        [TestMethod]
+        public void Input_1_ShouldRetrun_1()
+        {
+            var input = 1;
+            var expect = 1;
+            AssertShoudBe(expect, input);
+        }
+
+        [TestMethod]
+        public void Input_2_ShouldReturn_2()
+        {
+            var input =2;
+            var expect = 2;
+            AssertShoudBe(expect, input);
+        }
+
+
+        private void AssertShoudBe(int expect, int input)
+        {
+            var BC = new BitCounting();
+            int actual = BC.getNumberOfBit(input);
+            Assert.AreEqual(expect, input);
+        }
+    }
+
+    public class BitCounting
+    {
+        public int getNumberOfBit(int input)
+        {
+            return input;
+        }
+    }
+}
